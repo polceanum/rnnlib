c24d82089ad30cdc1f380c4824e8fd78d7ae22e6
Simon Hofmann <mail@simon-hofmann.org>
2016-07-14 17:47:55 +0200

    Sync

--- examples/online_prediction/build_netcdf.sh
+++ examples/online_prediction/build_netcdf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 . env.sh
-./offline_delta.py offline_training_set.txt offline.nc 100 100
-./offline_delta.py offline_validation_set.txt offline_validation.nc 100 100 
-#./online_delta.py training_set.txt online.nc
-#./online_delta.py validation_set.txt online_validation.nc
-#normalise_inputs.sh -c 2 online.nc online_validation.nc
-#normalise_targets.sh -c 2 online.nc online_validation.nc
+#./offline_delta.py offline_training_set.txt offline.nc 100 100
+#./offline_delta.py offline_validation_set.txt offline_validation.nc 100 100 
+./online_delta.py training_set.txt online.nc
+./online_delta.py validation_set.txt online_validation.nc
+normalise_inputs.sh -c 2 online.nc online_validation.nc
+normalise_targets.sh -c 2 online.nc online_validation.nc
